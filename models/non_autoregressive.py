649bea2aa343a3d28ce1495eae6819f2b2b20358
费政聪 <3300949577@qq.com>
2022-03-14 16:58:29 +0800

    Add files via upload

--- models/non_autoregressive.py
+++ models/non_autoregressive.py
@@ -1,8 +1,9 @@
 import torch 
 from torch import nn 
+from torch import Tensor
 
 from .containers import Module, ModuleList 
-from .transformer import DecoderLayer, sinusoid_encoding_table
+from .transformer import DecoderLayer, sinusoid_encoding_table, Encoder
 from .utils import one_hot_to_index 
 
 
@@ -75,3 +76,40 @@ class NAICDecoder(Module):
 
         out = self.fc(out)
         return out
+
+
+# input partially observed senetence with [mask] and predict the corresponding places 
+class NAICModel(Module): 
+    def __init__(self, config):
+        super(NAICModel, self).__init__() 
+        self.model_d = config.n_embd 
+        self.clip_dim = config.clip_dim 
+        self.clip_length = config.clip_length
+        self.feature_project = nn.Linear(config.clip_dim, config.clip_length*config.n_embd) 
+        self.visual_encoder = Encoder(config.n_layer, config.clip_length, config.n_embd) 
+        self.language_decoder = NAICDecoder(config.vocab_size) 
+
+        self.bos_idx = config.bos_token_id 
+        self.eos_idx = config.eos_token_id 
+        self.vocab_size = config.vocab_size 
+        self.max_generation_length = self.language_decoder.max_len 
+
+        self.register_state('enc_output', None)
+        self.register_state('mask_enc', None)
+        self.init_weights() 
+    
+    def init_weights(self):
+        for p in self.visual_encoder.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+        for p in self.language_decoder.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p) 
+    
+    def forward(self, images, seq):
+        images = self.feature_project(images).view(-1, self.clip_length, self.clip_dim)
+        enc_output, mask_enc = self.visual_encoder(images)
+        dec_output = self.language_decoder(seq, enc_output, mask_enc)
+        return dec_output 
+
+    
